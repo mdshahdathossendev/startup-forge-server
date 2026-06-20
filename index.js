5629511442540ca7d4cee1226ee5f1d41c6d5ae3
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-06-20 19:25:44 +0600

    applicatin rote add

--- index.js
+++ index.js
@@ -20,10 +20,25 @@ async function run() {
   try {
     const db = client.db("Start-Forges");
     const StartupsCollection = db.collection("startups Collection");
-    const OpportunitiesCollection = db.collection("opportunities Collection")
+    const OpportunitiesCollection = db.collection("opportunities Collection");
+    const ApplicationsCollection = db.collection("applications Collection");
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get('/application', async(req, res)=> {
+      const result = await ApplicationsCollection.find().toArray();
+      res.send(result)
+    });
+    app.post('/application', async (req, res) => {
+    const newApplication = req.body;
+    const result = await ApplicationsCollection.insertOne(newApplication);
+     res.send(result);
+    });
+    app.get('/application/:opportunity_id', async (req, res) => {
+    const { opportunity_id } = req.params;
+     const result = await ApplicationsCollection.find({opportunity_id: opportunity_id }).toArray();
+     res.send(result);
+});
     app.get('/opportunities', async(req, res)=> {
       const result = await OpportunitiesCollection.find().toArray();
       res.send(result)
@@ -41,6 +56,33 @@ async function run() {
     app.get('/opportunities/mange/:startupId', async (req, res) => {
      const startupId = req.params.startupId;
      const result = await OpportunitiesCollection.find({startup_id: startupId}).toArray();
+     res.send(result);
+   });
+   app.put('/opportunities/:id', async (req, res) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+
+  const result = await OpportunitiesCollection.updateOne(
+    { _id: new ObjectId(id) },
+    {
+      $set: {
+        role_title: updatedData.role_title,
+        required_skills: updatedData.required_skills,
+        work_type: updatedData.work_type,
+        commitment_level: updatedData.commitment_level,
+        date: updatedData.date,
+      },
+    }
+  );
+
+  res.send(result);
+});
+app.delete('/opportunities/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const result = await OpportunitiesCollection.deleteOne({
+    _id: new ObjectId(id),
+  });
 
   res.send(result);
 });
@@ -48,6 +90,15 @@ async function run() {
       const result = await StartupsCollection.find().toArray();
       res.send(result)
     });
+    app.delete('/startups/:email', async (req, res) => {
+  const email = req.params.email;
+
+  const result = await StartupsCollection.deleteOne({
+    founder_email: email,
+  });
+
+  res.send(result);
+});
     app.post('/startups', async(req, res)=> {
       const query = req.body;
       const result = await StartupsCollection.insertOne(query);
